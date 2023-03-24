6866b34098e1466706802f3e9b737a9d375af530
Samuel-Sbf <115561756+Samuel-Sbf@users.noreply.github.com>
2023-03-24 16:49:53 -0300

    Atualização do Script do carrosel

--- js/carrossel.js
+++ js/carrossel.js
@@ -3,21 +3,21 @@ window.onload = function () {
     let urlDiretorio = "https://Samuel-Sbf.github.io/estilo/"
     var cards = "";
     const slide = document.querySelector('.card-wrapper');
-
+    
     for (let x = 0; x != listaPessoas.length; x++) {
         cards += `
             <div class="card swiper-slide">
                 <div class="image-content">
                     <span class="overlay"></span>
-
+    
                     <div class="card-image">
                         <img src="${urlDiretorio}img/${listaPessoas[x]['slack']}.jpg" class="card-img">
                     </div>
                 </div>
-
+    
                 <div class="card-content">
                     <h2 class="name"> ${listaPessoas[x]['nome']} </h2>
-
+    
                     <p class="description">  </p>
                     
                     <div class="contact">
@@ -29,15 +29,15 @@ window.onload = function () {
                     </div>
                 </div>
             </div>
-            `;
+    `;
     }
-
+    
     slide.innerHTML = cards.toString();
-
+    
     setInterval(() => document.querySelector('.swiper-button-next').click(), 5000)
-}
-
-document.onkeyup = (x) => {
+    }
+    
+    document.onkeyup = (x) => {
     switch (x.key) {
         case "ArrowRight":
         case "ArrowDown":
@@ -47,8 +47,43 @@ document.onkeyup = (x) => {
         case "ArrowUp":
             document.querySelector('.swiper-button-prev').click()
             break;
-
+    
         default:
             break;
     }
-}
+    }
+    
+        var swiper = new Swiper(".slide-content", {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 25,
+            loop: true,
+            centerSlide: 'true',
+            fade: 'true',
+            grabCursor: 'true',
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+    
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                    slidesPerGroup: 1,
+                },
+                520: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                },
+                950: {
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                },
+            },
+        });
+    
